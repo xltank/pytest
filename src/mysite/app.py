48f77273f318d079cfe4ceb0ca2606bfd6be2d8b
xltank <xltank@gmail.com>
2013-01-28 01:14:53 -0800

    add session and cookie.

--- src/mysite/app.py
+++ src/mysite/app.py
@@ -7,6 +7,16 @@ Created on 2013-1-20
 
 import web
 import hashlib
+from web.session import Session, DiskStore
+import time
+from datetime import datetime
+import random
+from web.webapi import seeother
+
+
+web.config.debug = False
+
+web.config.session_parameters['timeout'] = 300
 
 render = web.template.render('templates/')
 urls = (
@@ -21,10 +31,27 @@ app = web.application(urls, globals())
 
 db = web.database(dbn='mysql', user='root', pw='root', db='test')
 
+ses = Session(app, DiskStore('sessions'), initializer={'userName':'', 'token':''})
+
+
+def checkToken():
+    un = web.cookies().get('userName')
+    tk = web.cookies().get('token')
+    if(un and tk and ses):
+        if(un == ses.userName and tk == ses.token):
+            return True
+    raise web.seeother('/login')
+
+def setCookieSession(un, pw):
+    ses.userName = un
+    ses.token = hashlib.md5(pw + str(random.randint(0, 99999999))).hexdigest()
+    web.setcookie('userName', un, 300)
+    web.setcookie('token', ses.token, 300)
 
 class index:
     def GET(self):
-        return render.index()
+        if(checkToken() == True):
+            raise web.seeother('/userlist')
 
 class login:
     def GET(self):
@@ -33,21 +60,26 @@ class login:
     def POST(self):
         f = web.input()
         if(f.name and f.password):
-            f.password = hashlib.md5(f.password).hexdigest()
-            r = db.query("select * from user where name=$name and password=$password", vars=dict(f))
+            d = dict(f)
+            d['pw'] = hashlib.md5(f.password).hexdigest()
+            r = db.query("select * from user where name=$name and password=$pw", vars=d)
         if(len(r) > 0):
+            setCookieSession(f.name, f.password)
             web.seeother('/userlist')
         else:
-            raise web.seeother('/')
+            raise web.seeother('/login')
 
 
 class userlist:
     def GET(self):
-        items = db.select('user')
-        return render.userlist(items)
+        if(checkToken() == True):
+            items = db.select('user')
+            return render.userlist(items)
 
 class logout:
     def GET(self):
+        ses.kill()
+        web.setcookie('token', '')
         return render.index()
 
 class signup:
@@ -58,9 +90,11 @@ class signup:
         i = web.input()
         if(i.name and i.password and i.password == i.confirm):
             pswdHash = hashlib.md5(i.password).hexdigest()
-            db.insert('user', name=i.name, password=pswdHash, description=i.description)
-        raise web.seeother('/signup')
+            db.insert('user', name=i.name, password=pswdHash, description=i.description, creationTime=datetime.now())
+            setCookieSession(i.name, i.password)
+            raise web.seeother('/userlist')
 
 
 if __name__ == '__main__':
+#    app.add_processor(web.loadhook(checkToken))
     app.run()
